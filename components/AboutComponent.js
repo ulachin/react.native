6a442323e2d733a2da847d01c131a10805e6e8fa
Ula Chin <ula.chin092@gmail.com>
2021-11-03 21:45:21 -0400

    cakepops

---
+++ components/AboutComponent.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import ScrollView from "react-natve";
+
+class About extends Component{
+    render() {
+        return(
+            <ScrollView>
+
+            </ScrollView>
+        )
+    }
+
+}
+
+export default About;
---
+++ components/ContactComponent.js
@@ -0,0 +1,17 @@
+import React,{Component} from "react";
+import {ScrollView} from "react-natve";
+
+
+class Contact extends Component{
+    render() {
+        return(
+            <ScrollView>
+
+            </ScrollView>
+        )
+    }
+
+}
+
+
+export default Contact; 

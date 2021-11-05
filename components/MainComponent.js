da638d4e9351da031499be1c3613edda212a1699
Ula Chin <ula.chin092@gmail.com>
2021-11-05 01:57:34 -0400

    datepicker

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -14,6 +14,7 @@ import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions,
     fetchPartners } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';    
 
 const mapDispatchToProps = {
         fetchCampsites,
@@ -121,6 +122,29 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView 
@@ -167,7 +191,23 @@ const MainNavigator = createDrawerNavigator(
                 )
             }
         },
-        About: {
+        
+    Reservation: {
+        screen: ReservationNavigator,
+        navigationOptions: {
+            drawerLabel: 'Reserve Campsite',
+            drawerIcon: ({tintColor}) => (
+                <Icon
+                    name='tree'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
+        }
+    },
+
+    About: {
             screen: AboutNavigator,
             navigationOptions: {
                 drawerLabel: 'About Us',
